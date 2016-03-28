91a065051bb149272fc15781285436a81e934d5f
Cameron Hart <cameron.hart@gmail.com>
2016-03-28 16:12:26 +1100

    Formatting

--- c/timer_impl_windows.c
+++ c/timer_impl_windows.c
@@ -8,21 +8,21 @@
 
 void bits_time_init(BitsTime * time)
 {
-	LARGE_INTEGER frequency;
-	LARGE_INTEGER delta;
+    LARGE_INTEGER frequency;
+    LARGE_INTEGER delta;
 
-	QueryPerformanceFrequency(&frequency); 
-	QueryPerformanceCounter(&delta);
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&delta);
 
-	time->storage = (uint64_t)(TO_NANOSECS * delta.QuadPart / frequency.QuadPart);
+    time->storage = (uint64_t)(TO_NANOSECS * delta.QuadPart / frequency.QuadPart);
 }
 
 
 double bits_time_delta_secs(const BitsTime * time)
 {
-	BitsTime now;
-	bits_time_init(&now);
-	return (double)(now.storage - time->storage) / TO_NANOSECS;
+    BitsTime now;
+    bits_time_init(&now);
+    return (double)(now.storage - time->storage) / TO_NANOSECS;
 }
 
 #endif // WIN32
