674b62e5d6ec9f7b7dcd119ecbc1cf03c498c9c6
Cameron Hart <cameron.hart@gmail.com>
2016-11-16 15:53:29 +1100

    MSVC fixes.
    std::arrays were blowing the stack.
    lambda captured variables weren't living long enough.

--- bench/main.cpp
+++ bench/main.cpp
@@ -1,5 +1,7 @@
 #include "radixsort.hpp"
+#if HAVE_C99_SUPPORT
 #include "radixsort.h"
+#endif
 
 #define NONIUS_RUNNER
 #include <nonius.hpp>
@@ -32,15 +34,17 @@ template <typename KeyType>
 struct InitData
 {
     static const uint32_t MAX_SIZE = 65536;
-    std::array<KeyType, MAX_SIZE> keys_;
-    std::array<uint32_t, MAX_SIZE> values_;
+    std::vector<KeyType> keys_;
+    std::vector<uint32_t> values_;
 
     template <typename Rand>
     InitData(Rand& rand)
+        : keys_(MAX_SIZE)
+        , values_(MAX_SIZE)
     {
         for (uint32_t i = 0; i < MAX_SIZE; ++i)
         {
-            keys_[i] = rand();
+            keys_[i] = static_cast<KeyType>(rand());
             values_[i] = i;
         }
     }
@@ -53,23 +57,27 @@ struct InitData
 
 struct BenchmarkRegistrar
 {
+    // mersenne twister prng
+    std::mt19937 rnd32;
+    std::mt19937_64 rnd64;
+
+    const InitData<uint32_t> uint32_data;
+    const InitData<uint64_t> uint64_data;
+    const InitData<float> float_data;
 
     BenchmarkRegistrar()
+        : uint32_data(rnd32)
+        , uint64_data(rnd64)
+        , float_data(rnd32)
     {
         const uint32_t start = 64, end = 65536, inc = 2;
 
-        // mersenne twister prng
-        std::mt19937 rnd32;
-        std::mt19937_64 rnd64;
-
         char name[1024];
         auto& registry = nonius::global_benchmark_registry();
-
-        const InitData<uint32_t> uint32_data(rnd32);
         for (uint32_t size = start; size <= end; size = size << inc)
         {
             sprintf(name, "%d uint32_t key bits::radix8sort", size);
-            registry.emplace_back(name, [&uint32_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint32_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
@@ -77,17 +85,19 @@ struct BenchmarkRegistrar
                             });
                     });
 
+#if HAVE_C99_SUPPORT
             sprintf(name, "%d uint32_t key radix8sort_u32", size);
-            registry.emplace_back(name, [&uint32_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint32_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
                             return radix8sort_u32(keys0, keys1, values0, values1, size);
                             });
                     });
+#endif
 
             sprintf(name, "%d uint32_t key bits::radix11sort", size);
-            registry.emplace_back(name, [&uint32_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint32_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
@@ -95,17 +105,19 @@ struct BenchmarkRegistrar
                             });
                     });
 
+#if HAVE_C99_SUPPORT
             sprintf(name, "%d uint32_t key radix11sort_u32", size);
-            registry.emplace_back(name, [&uint32_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint32_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
                             return radix11sort_u32(keys0, keys1, values0, values1, size);
                             });
                     });
+#endif
 
             sprintf(name, "%d uint32_t key std::sort", size);
-            registry.emplace_back(name, [&uint32_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint32_data.run_data(size);
                     meter.measure([first=std::begin(data.keys0_), last=std::end(data.keys0_)] {
                             std::sort(first, last);
@@ -114,11 +126,10 @@ struct BenchmarkRegistrar
                     });
         }
 
-        const InitData<uint64_t> uint64_data(rnd64);
         for (uint32_t size = start; size <= end; size = size << inc)
         {
             sprintf(name, "%d uint64_t key bits::radix8sort", size);
-            registry.emplace_back(name, [&uint64_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint64_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
@@ -126,17 +137,19 @@ struct BenchmarkRegistrar
                             });
                     });
 
+#if HAVE_C99_SUPPORT
             sprintf(name, "%d uint64_t key radix8sort_u64", size);
-            registry.emplace_back(name, [&uint64_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint64_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
                             return radix8sort_u64(keys0, keys1, values0, values1, size);
                             });
                     });
+#endif
 
             sprintf(name, "%d uint64_t key bits::radix11sort", size);
-            registry.emplace_back(name, [&uint64_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint64_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
@@ -144,17 +157,19 @@ struct BenchmarkRegistrar
                             });
                     });
 
+#if HAVE_C99_SUPPORT
             sprintf(name, "%d uint64_t key radix11sort_u64", size);
-            registry.emplace_back(name, [&uint64_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint64_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
                             return radix11sort_u64(keys0, keys1, values0, values1, size);
                             });
                     });
+#endif
 
             sprintf(name, "%d uint64_t key std::sort", size);
-            registry.emplace_back(name, [&uint64_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = uint64_data.run_data(size);
                     meter.measure([first=std::begin(data.keys0_), last=std::end(data.keys0_)] {
                             std::sort(first, last);
@@ -163,11 +178,10 @@ struct BenchmarkRegistrar
                     });
         }
 
-        const InitData<float> float_data(rnd32);
         for (uint32_t size = start; size <= end; size = size << inc)
         {
             sprintf(name, "%d float key bits::radix8sort", size);
-            registry.emplace_back(name, [&float_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = float_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
@@ -175,17 +189,19 @@ struct BenchmarkRegistrar
                             });
                     });
 
+#if HAVE_C99_SUPPORT
             sprintf(name, "%d float key radix8sort_f32", size);
-            registry.emplace_back(name, [&float_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = float_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
                             return radix8sort_f32(keys0, keys1, values0, values1, size);
                             });
                     });
+#endif
 
             sprintf(name, "%d float key bits::radix11sort", size);
-            registry.emplace_back(name, [&float_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = float_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
@@ -193,17 +209,19 @@ struct BenchmarkRegistrar
                             });
                     });
 
+#if HAVE_C99_SUPPORT
             sprintf(name, "%d float key radix11sort_f32", size);
-            registry.emplace_back(name, [&float_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = float_data.run_data(size);
                     meter.measure([keys0=data.keys0(), keys1=data.keys1(),
                             values0=data.values0(), values1=data.values1(), size] {
                             return radix11sort_f32(keys0, keys1, values0, values1, size);
                             });
                     });
+#endif
 
             sprintf(name, "%d float key std::sort", size);
-            registry.emplace_back(name, [&float_data, size](nonius::chronometer meter) {
+            registry.emplace_back(name, [this, size](nonius::chronometer meter) {
                     auto data = float_data.run_data(size);
                     meter.measure([first=std::begin(data.keys0_), last=std::end(data.keys0_)] {
                             std::sort(first, last);
